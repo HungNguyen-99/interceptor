63927177f1d6cee73893083ebbe5380e5c888ec5
Hung <hungnvhe130726@fpt.edu.vn>
2022-02-13 09:07:32 +0700

    commit all interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServiceJsonService } from './service-json.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(private dataService: ServiceJsonService){}
+
+  ngOnInit(): void {
+    this.dataService.getMovies().subscribe((data)=>{
+      console.log('appComponent', data);
+    });
+  }
 }
---
+++ src/app/service-json.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceJsonService {
+
+  private REST_API_SERVER = 'http://localhost:3000/movies';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getMovies(): Observable<any>{
+    return (
+      this.httpClient.get(this.REST_API_SERVER).pipe(catchError(this.handleError))
+    );
+  } 
+  public getMovies2(): Observable<any>{
+    return (
+      this.httpClient.get(this.REST_API_SERVER).pipe(catchError(this.handleError))
+    );
+  } 
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
+
+
